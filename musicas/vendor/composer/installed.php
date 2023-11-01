696f0085a99269db983e59adbfbbf030a5b62dd9
Maria Clara Nazareno Aires <claranaires@gmail.com>
2023-11-01 04:56:32 -0300

    feat: configuração do composer - camada de apresentação

---
+++ musicas/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '55639/myplaylist',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '309be3b2cb37240f7ed231f01e3c6b0163bd31e8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '55639/myplaylist' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '309be3b2cb37240f7ed231f01e3c6b0163bd31e8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
